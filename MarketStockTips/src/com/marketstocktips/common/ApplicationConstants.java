96245529a0bbb75f87e7789f20c11c987f5a1f1c
pavi1873 <pavi1873@gmail.com>
2014-04-17 17:12:53 +0530

    Base line Code
    Base line Code

---
+++ MarketStockTips/src/com/marketstocktips/common/ApplicationConstants.java
@@ -0,0 +1,14 @@
+package com.marketstocktips.common;
+
+public class ApplicationConstants {
+	
+	public static String APPLICATION_DIRECTORY_PATH = "C:\\Pavi\\Personal\\AutoIt\\Personal Scripts\\MarketStockTips\\";
+	public static String APPLICATION_EXECUTABLE_FILE ="CheckTips.exe";
+	public static String TIP_DATA_DIRECTORY_PATH = "C:\\Pavi\\Development\\Tips\\";
+	public static String TIP_DATA_FILE_PATH = "Tips.txt";
+	public static String TIP_TEMPLATE_DATA_DIRECTORY_PATH = "C:\\Pavi\\Development\\Tips\\";
+	public static String TIP_TEMPLATE_DATA_FILE_PATH = "Tips_template.txt";
+
+	
+}
+
